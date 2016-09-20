a45e3aa884db4f3e6e1b0a4ca0a1b0560c699a5e
Roman Belkov <romanbelkov@gmail.com>
2016-09-20 11:22:09 +0300

    Added SPBU .NET 2.2

---
+++ SPBU/dotNet/2.2/Palindrome/PalindromeDetector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Palindrome
+{
+    internal class PalindromeDetector
+    {
+        private static bool IsDelimiter(char c)
+        {
+            return !Char.IsLetterOrDigit(c);
+        }
+
+        private static bool CharEquality(char a, char b)
+        {
+            return Char.ToLower(a).Equals(Char.ToLower(b));
+        }
+
+        public static bool IsPalindrome(string s)
+        {
+            var i = 0;
+            var j = s.Length - 1;
+
+            do
+            {
+                while (IsDelimiter(s[i])) i++;
+                while (IsDelimiter(s[j])) j--;
+                if (!CharEquality(s[i], s[j]))
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            } while (i < j - 1);
+            return true;
+
+        }
+    }
+}
---
+++ SPBU/dotNet/2.2/Palindrome/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Type a string:");
+            var s = Console.ReadLine();
+            Console.WriteLine(PalindromeDetector.IsPalindrome(s) ? "Palindrome" : "Not a palindrome");
+        }
+    }
+}
+
+
